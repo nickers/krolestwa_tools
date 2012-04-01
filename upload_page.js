8c80a69b42985f34f62cd69555819f8a6160ae86
Tomasz Wsuł <2nickers@gmail.com>
2012-04-01 17:16:13 +0200

    base uploading

---
+++ upload_page.js
@@ -0,0 +1,18 @@
+(function() {
+	var data = 'data=' + document.documentElement.innerHTML;
+
+	var url = "http://localhost:9999/";
+	http.open("POST", url, true);
+
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("Content-length", data.length);
+	http.setRequestHeader("Connection", "close");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+		if(http.readyState == 4 && http.status == 200) {
+			alert(http.responseText);
+		}
+	}
+	http.send(data);
+}())
